bb838fdcfe34e9c25860a7f30e50d0e7196e8146
JackMal157 <malates5@msu.edu>
2025-04-21 16:35:23 +0000

    Adding tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 
 function App() {
   return (
@@ -6,10 +6,12 @@ function App() {
       <div className="page">
         <header className="header">
           <h1 className = "title">CYTC</h1>
+          <h2 className = "yellow">Custom Youtube Cinema</h2>
         </header>
         <div className="body">
           <h3 className="KeyWords">Enter Key Search Words:</h3>
           <h3 className="DateRange">Enter Date Range Here:</h3>
+          <button className="GenerateButton">Generate Video</button>
         </div>
       </div>
     </>  
