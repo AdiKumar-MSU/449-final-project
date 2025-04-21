9ed6f21ecd6fd597344ca76a95868072c714f9dd
AdiKumar-MSU <143560324+AdiKumar-MSU@users.noreply.github.com>
2025-04-21 16:42:33 +0000

    search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_KEY = 'AIzaSyBXj-YM_wRez3b063oxWhk924kIXMP0Ff0'
 
-  return (
+function KeywordSearch() {
+  const [videos, setVideos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [prevTerm, setPrevTerm] = useState('');
+  const [displayVid, setDisplayVid] = useState('');
+  
+  const searchYT = async (searchTerm) => {
+
+    const res = await Axios.get(
+      'https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          part: 'snippet',
+          q: searchTerm,
+          type: 'video',
+          maxResults: 6,
+          key: API_KEY
+        }
+      }
+    );
+    setVideos(res.data.items);
+    setPrevTerm(searchTerm);
+    
+    if (res.data.items.length > 0) {
+      const randomIndex = Math.floor(Math.random() * res.data.items.length);
+      setDisplayVid(res.data.items[randomIndex]);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim() != prevTerm.trim()) {
+      searchYT(searchTerm);
+    } else {
+      if (videos.length > 0) {
+        const randomIndex = Math.floor(Math.random() * videos.length);
+        setDisplayVid(videos[randomIndex]);
+      }
+    }
+  };
+
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="videoSearch">
+        <form onSubmit={handleSubmit}>
+          <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search YouTube"
+          />
+          <button type="submit">
+            Search
+          </button>
+        </form>
+        
 
-export default App
+        <div className="video-list">
+          {videos.length > 0 && (
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${displayVid.id.videoId}`}
+              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+export default KeywordSearch
